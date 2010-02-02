699660bb75f79392173fdd3d432dcb8fd00dad68
Christoph Hochstrasser <christoph.hochstrasser@googlemail.com>
2010-02-02 18:16:27 +0100

    Initial Commit

---
+++ application/commands/DefaultCommand.php
@@ -0,0 +1,12 @@
+class DefaultCommand implements Spark_Controller_CommandInterface
+{
+
+  public function execute(
+    Spark_Controller_RequestInterface $request, 
+    Zend_Controller_Response_Abstract $response
+  )
+  {
+
+  }
+
+}
---
+++ application/commands/PageCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+class PageCommand implements Spark_Controller_CommandInterface
+{
+  
+  public function execute(
+    Spark_Controller_RequestInterface $request, 
+    Zend_Controller_Response_Abstract $response
+  )
+  {
+  
+  }
+  
+}
