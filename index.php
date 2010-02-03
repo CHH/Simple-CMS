8553e1b2707bb9ab0875893c8033735576a59364
Christoph Hochstrasser <christoph.hochstrasser@googlemail.com>
2010-02-03 20:48:51 +0100

    Added ability to use a common layout for all pages.

--- index.php
+++ index.php
@@ -2,13 +2,33 @@
 
 define("WEBROOT", realpath(dirname(__FILE__)));
 
+define("CONFIGS", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . "config"));
+
 define("APPLICATION_PATH", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR 
        . "application"));
 
 define("LIBRARY_PATH", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . "library"));
 
-define("SPARK_PATH", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 
-  "lib" . DIRECTORY_SEPARATOR . "Spark" . DIRECTORY_SEPARATOR . "lib"));
+$coreConfig = new Zend_Config_Ini(CONFIGS . DIRECTORY_SEPARATOR . "core.ini");
+
+if(isset($coreConfig->framework->path) and is_dir($coreConfig->framework->path)) {
+  define("SPARK_PATH", $coreConfig->framework->path);
+} else {
+  // Assume a cloned Spark Repo in the parent folder
+  $sparkPath = realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".." .
+    DIRECTORY_SEPARATOR . "Spark-Web-Framework" . DIRECTORY_SEPARATOR . "lib");
+  
+  if(is_dir($sparkPath)) {
+    define("SPARK_PATH", $sparkPath);
+  } else {
+    throw new Exception("Simple-CMS depends on the Spark-Web-Framework. 
+     Make sure who have either cloned the Git Repository 
+     (git clone git://github.com/yuri41/Spark-Web-Framework.git) 
+     in the parent folder of your Simple-CMS installation 
+     or you have configured the framework.path directive 
+     in the core.ini file correctly.");
+  }
+}
 
 set_include_path(LIBRARY_PATH . PATH_SEPARATOR . SPARK_PATH . PATH_SEPARATOR . get_include_path());
 
@@ -17,12 +37,10 @@ function __autoload($class)
   include_once str_replace("_", DIRECTORY_SEPARATOR, $class) . ".php";
 }
 
-$resolverOptions = array(
-  "command_directory" => APPLICATION_PATH . DIRECTORY_SEPARATOR . "commands",
-);
+// Write the Core Config to the Registry
+Spark_Registry::set("CoreConfig", $coreConfig);
 
-Spark_Object_Manager::addBinding(Spark_Object_OptionBinding::staticBind($resolverOptions)
-  ->to("Spark_Controller_CommandResolver"));
+Spark_Object_Manager::setConfig($coreConfig);
 
 $frontController = Spark_Object_Manager::getInstance("Spark_Controller_FrontController");
 
