64c760a71ec648234fd474e5112c61490f843e30
Christoph Hochstrasser <christoph.hochstrasser@googlemail.com>
2010-04-02 16:51:41 +0200

    Added more docs to classes

--- library/Plugin.php
+++ library/Plugin.php
@@ -2,68 +2,139 @@
 
 abstract class Plugin implements PluginInterface
 {
-  
+  /**
+   * @var array
+   */
   protected $_vars = array();
   
+  /**
+   * @var Spark_Controller_FrontController
+   */
   protected $_frontController = null;
   
+  /**
+   * @var Spark_Controller_Filter_ApplyLayout
+   */
   protected $_layoutFilter = null;
   
-  protected $_config = null;
+  /**
+   * @var array
+   */
+  protected $_config = array();
   
   public function __construct($config = null)
   {
     $this->setConfig($config);
   }
   
+  /**
+   * bootstrap() - Gets called by the main bootstrap when the plugin gets loaded
+   */
   public function bootstrap()
   {}
   
+  /**
+   * beforeDispatch() - FrontController Callback, gets called before routing is done
+   */
   public function beforeDispatch()
   {}
   
+  /**
+   * afterDispatch() - FrontController Callback, gets called after routing
+   */
   public function afterDispatch()
   {}
   
+  /**
+   * setFrontController() - Set FrontController for the Plugin
+   *
+   * @param Spark_Controller_FrontController $frontController
+   * @return Plugin
+   */
   public function setFrontController(Spark_Controller_FrontController $frontController)
   {
     $this->_frontController = $frontController;
     return $this;
   }
   
+  /**
+   * getFrontController() - Returns the FrontController
+   *
+   * @return Spark_Controller_FrontController
+   */
   public function getFrontController()
   {
     return $this->_frontController;
   }
   
+  /**
+   * setLayoutFilter() - Sets the layout filter to the plugin, so plugins can modify the layout
+   * (Set placeholder values, layout variables, view helpers and so on)
+   *
+   * @param Spark_Controller_Filter_ApplyLayout $layoutFilter
+   * @return Plugin
+   */
   public function setLayoutFilter(Spark_Controller_Filter_ApplyLayout $layoutFilter)
   {
     $this->_layoutFilter = $layoutFilter;
     return $this;
   }
   
+  /**
+   * getLayoutFilter() - Returns the LayoutFilter instance
+   * 
+   * @return Spark_Controller_Filter_ApplyLayout
+   */
   public function getLayoutFilter()
   {
     return $this->_layoutFilter;
   }
   
+  /**
+   * setConfig() - Sets a config to the plugin
+   *
+   * @param mixed $config
+   * @return Plugin
+   */
   public function setConfig($config)
   {
+    if($config instanceof Zend_Config_Ini) {
+      $config = $config->toArray();
+    }
+    
     $this->_config = $config;
     return $this;
   }
   
+  /**
+   * getConfig() - Returns the plugin config
+   *
+   * @return array
+   */
   public function getConfig()
   {
     return $this->_config;
   }
   
+  /**
+   * set() - Sets a key value pair, which can be accessed like a object property
+   *
+   * @param string $var
+   * @param mixed $value
+   * @return Plugin
+   */
   public function set($var, $value)
   {
     $this->_vars[$var] = $value;
     return $this;
   }
   
+  /**
+   * get() - Returns the value for the given key
+   *
+   * @param string $var
+   * @return mixed
+   */
   public function get($var)
   {
     if(array_key_exists($var, $this->_vars)) {
@@ -73,11 +144,23 @@ abstract class Plugin implements PluginInterface
     throw new Exception("You tried to access the property {$var}, but it hasn't been set");
   }
   
+  /**
+   * __set() - Alias for set(), enables $this->$var = $value assignments
+   * @see set()
+   * @param string $var
+   * @param mixed $value
+   */
   public function __set($var, $value)
   {
     $this->set($var, $value);
   }
   
+  /**
+   * __get - Alias for get()
+   * @see get()
+   * @param string $var
+   * @return mixed
+   */
   public function __get($var)
   {
     return $this->get($var);
--- library/PluginInterface.php
+++ library/PluginInterface.php
@@ -2,19 +2,57 @@
 
 interface PluginInterface
 {
-  
+  /**
+   * bootstrap() - Gets called from the main bootstrap at plugin load time
+   */
   public function bootstrap();
   
+  /** 
+   * setFrontController() - Provides an instance of the FrontController to the Plugin
+   * @var Spark_Controller_FrontController $frontController
+   * @return PluginInterface Should provide a fluent interface
+   */
   public function setFrontController(Spark_Controller_FrontController $frontController);
   
+  /**
+   * setLayoutFilter() - Provides an instance of the Layout to the Plugin,
+   * commonly used to change the Layout, set placeholders, add View Helpers,...
+   *
+   * @param Spark_Controller_Filter_ApplyLayout $layoutFilter
+   * @return PluginInterface Should provide a fluent interface
+   */
   public function setLayoutFilter(Spark_Controller_Filter_ApplyLayout $layoutFilter);
   
+  /**
+   * set() - Sets a key value pair to the Plugin
+   *
+   * @param string $var
+   * @param mixed $value
+   * @return PluginInterface Should provide a fluent interface
+   */
   public function set($var, $value);
   
+  /**
+   * get() - Gets the value for a given key
+   *
+   * @param string $var
+   * @return mixed
+   */
   public function get($var);
   
+  /**
+   * setConfig() - Sets a plugin specific config
+   *
+   * @param mixed $config Config file, should either be an array or an instance of Zend_Config
+   * @return PluginInterface Should provide a fluent interface
+   */
   public function setConfig($config);
   
+  /**
+   * getConfig() - Returns the plugin config
+   *
+   * @return array
+   */
   public function getConfig();
   
 }
