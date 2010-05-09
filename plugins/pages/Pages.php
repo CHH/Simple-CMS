52607d0fb1f0713c3350b9d01fa0c60154380391
Christoph Hochstrasser <christoph.hochstrasser@googlemail.com>
2010-05-09 15:49:39 +0200

    Some naming tweaks in the pages plugin's autoloaders

--- plugins/pages/Pages.php
+++ plugins/pages/Pages.php
@@ -5,26 +5,24 @@ class Pages extends Plugin
   
   public function bootstrap()
   {
-    spl_autoload_register(array($this, "autoloadPluginLibrary"));
-    spl_autoload_register(array($this, "autoloadPluginModel"));
+    spl_autoload_register(array($this, "autoloadPagesLibraries"));
+    spl_autoload_register(array($this, "autoloadPagesModels"));
     
-    $this->frontController->getRouter()->addRoute("pages", Spark_Object_Manager::create("PageRoute", array("module_name"=>"pages")));
+    $this->frontController->getRouter()->addRoute(
+      "pages", 
+      Spark_Object_Manager::create("PageRoute", array("module_name"=>"pages"))
+    );
     
     PageMapper::setDefaultRenderer($this->layout->getLayout());
   }
   
-  public function afterDispatch()
-  {
-    
-  }
-  
-  public function autoloadPluginLibrary($class)
+  public function autoloadPagesLibraries($class)
   {
     @include_once($this->getPath() . DIRECTORY_SEPARATOR . "library" . 
        DIRECTORY_SEPARATOR . str_replace("_", DIRECTORY_SEPARATOR, $class) . ".php");
   }
   
-  public function autoloadPluginModel($class)
+  public function autoloadPagesModels($class)
   {
      @include_once($this->getPath() . DIRECTORY_SEPARATOR . "models" . 
        DIRECTORY_SEPARATOR . str_replace("_", DIRECTORY_SEPARATOR, $class) . ".php");
