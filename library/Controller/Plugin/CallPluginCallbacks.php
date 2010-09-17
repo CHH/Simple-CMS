76f8e922cc35dc409d26775d61aeeacd37620954
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2010-09-17 13:55:39 +0200

    Code formatting

--- library/Controller/Plugin/CallPluginCallbacks.php
+++ library/Controller/Plugin/CallPluginCallbacks.php
@@ -4,58 +4,56 @@ class Controller_Plugin_CallPluginCallbacks
     extends Spark_Controller_AbstractPlugin
     implements Spark_Configurable
 {
-  protected $_plugins;
-  
-  public function __construct(Array $options = array()) 
-  {
-    $this->setOptions($options);
-  }
-  
-  public function setOptions(Array $options)
-  {
-    Spark_Options::setOptions($this, $options);
-    return $this;
-  }
-  
-  public function beforeDispatch($request, $response)
-  {
-    $plugin = $this->_getPluginName($request);
+    protected $_plugins;
 
-    if($this->_plugins->has($plugin)) {
-      $plugin = $this->_plugins->get($plugin);
-      
-      if (!is_callable(array($plugin, "beforeDispatch"))) {
-        return;
-      }
-      
-      $plugin->beforeDispatch($request, $response);
+    public function __construct(Array $options = array()) 
+    {
+       $this->setOptions($options);
     }
-  }
-  
-  public function afterDispatch($request, $response)
-  {
-    $plugin = $this->_getPluginName($request);
-    
-    if($this->_plugins->has($plugin)) {
-      $plugin = $this->_plugins->get($plugin);
-      
-      if (!is_callable(array($plugin, "afterDispatch"))) {
-        return;
-      }
-      
-      $plugin->afterDispatch($request, $response);
+
+    public function setOptions(Array $options)
+    {
+        Spark_Options::setOptions($this, $options);
+        return $this;
     }
-  }
-  
-  public function setPlugins(Spark_Registry $plugins)
-  {
-    $this->_plugins = $plugins;
-    return $this;
-  }
-  
-  protected function _getPluginName($request)
-  {
-    return str_replace(" ", null, ucwords(str_replace("_", " ", $request->getModuleName())));
-  }
-  
+
+    public function beforeDispatch($request, $response)
+    {
+        $plugin = $this->_getPluginName($request);
+
+        if($this->_plugins->has($plugin)) {
+            $plugin = $this->_plugins->get($plugin);
+
+            if (!is_callable(array($plugin, "beforeDispatch"))) {
+                return;
+            }
+            $plugin->beforeDispatch($request, $response);
+        }
+    }
+
+    public function afterDispatch($request, $response)
+    {
+        $plugin = $this->_getPluginName($request);
+
+        if($this->_plugins->has($plugin)) {
+            $plugin = $this->_plugins->get($plugin);
+
+            if (!is_callable(array($plugin, "afterDispatch"))) {
+                return;
+            }
+
+            $plugin->afterDispatch($request, $response);
+        }
+    }
+
+    public function setPlugins(Spark_Registry $plugins)
+    {
+        $this->_plugins = $plugins;
+        return $this;
+    }
+
+    protected function _getPluginName($request)
+    {
+        return str_replace(" ", null, ucwords(str_replace("_", " ", $request->getModuleName())));
+    }
 }
